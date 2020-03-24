4e0b343112449b1f1dcd0e322db85a14a4053ed4
Avi <avidrucker@gmail.com>
2020-03-24 11:35:46 -0400

    feat add child component roughs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 
 const Tweet = props => <div>{props.children}</div>
 
+const Avatar = () => <div>Avatar</div>;
+	
+const Message = () => <span>Insightful Message</span>;
+
+const HandleName = () => <span>@Handle Name</span>;
+
+const TimePosted = () => <span>3h ago</span>;
+
+const Button = () => <span>btn</span>;
+
 function App() {
   return (
     <div className="App">
       <Tweet>
-				I am a tweet!
+				<Avatar />
+				<Message />
+				<HandleName />
+				<TimePosted />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
 			</Tweet>
     </div>
   );
