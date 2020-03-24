b0e4acc4d139159eb926477d153c07a5c276d82e
Avi <avidrucker@gmail.com>
2020-03-24 13:40:46 -0400

    feat add props, layout, styles

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
 import React from 'react';
 import './App.css';
 
-const Tweet = props => <div>{props.children}</div>
+const Tweet = props => (
+	<div className={props.className}>{props.children}</div>);
 
-const Avatar = () => <div>Avatar</div>;
+const Avatar = props => <span>{props.symbolIn}</span>;
 	
-const Message = () => <span>Insightful Message</span>;
+const Message = (props) => <span>{props.textIn}</span>;
 
-const NameWithHandle = () => <span>@Handle Name</span>;
+const NameWithHandle = (props) => <span>{props.textIn}</span>;
 
-const Time = () => <span>3h ago</span>;
+const Time = (props) => <span>{props.textIn}</span>;
 
-const Button = () => <span>btn</span>;
+const Button = (props) => <span>{props.symbolIn}</span>;
+
+const Inline = props => (
+	<span className={props.className + " di"}>{props.children}</span>);
 
 const InlineBlock = props => (
-	<div
-		className={props.className}
-		styles={{"diplay":"inline-block"}}>
+	<span className={props.className + " dib"}>
 		{props.children}
-	</div>
+	</span>
 );
 	
-const Block = props => <div className={props.className}>{props.children}</div>;
+const Block = props => (
+	<div className={props.className + " db"}>{props.children}</div>);
 
 function App() {
+	const symbols = ["<--", "@", "<3", "..."];
+	const buttons = symbols.map((x,i) =>
+		<Inline key={i.toString()} className="bg-washed-red mh3">
+			<Button symbolIn={x} />
+		</Inline>
+  );
+
   return (
-    <div className="App">
-      <Tweet>
-				<InlineBlock>
-					<Avatar />
-				</InlineBlock>
-				<InlineBlock>
-					<Block>
-						<NameWithHandle />
-						<Time />
-						<Message />
+    <div className="f3 border-box">
+      <Tweet className="flex pa3 measure-narrow ba b--black ma3">
+
+				<InlineBlock className="w-20 flex flex-column items-center ">
+					<Block className="bg-lightest-blue tc f1 w3 h3 pa1">
+						<Avatar symbolIn={"&"} />
 					</Block>
-					<Block>
-						<Button />
-						<Button />
-						<Button />
-						<Button />
+				</InlineBlock>
+
+				<InlineBlock className="w-80 f4">
+					<Block className="flex flex-row">
+						<InlineBlock className="bg-washed-green">
+							<NameWithHandle textIn="Your Name @Handle Name" />
+						</InlineBlock>
+						<InlineBlock>
+							<Inline className="ma3">•</Inline>
+						</InlineBlock>
+						<InlineBlock className="bg-washed-yellow">
+							<Time textIn="3h ago"/>
+						</InlineBlock>
+					</Block>
+					<Block className="mv2 pv1">
+						<InlineBlock className="bg-washed-purple">
+							<Message textIn="Insightful message" />
+						</InlineBlock>
+					</Block>
+					<Block className="flex">
+						{buttons}
 					</Block>
 				</InlineBlock>
 			</Tweet>
