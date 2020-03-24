f5cae7d58918992410105bf852a790481b1407cb
Avi <avidrucker@gmail.com>
2020-03-24 11:47:10 -0400

    fix rename components, fix css error

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ const Avatar = () => <div>Avatar</div>;
 	
 const Message = () => <span>Insightful Message</span>;
 
-const HandleName = () => <span>@Handle Name</span>;
+const NameWithHandle = () => <span>@Handle Name</span>;
 
-const TimePosted = () => <span>3h ago</span>;
+const Time = () => <span>3h ago</span>;
 
 const Button = () => <span>btn</span>;
 
 const InlineBlock = props => (
-	<div styles={{"diplay":"inlineBlock"}}>{props.children}</div>
+	<div styles={{"diplay":"inline-block"}}>{props.children}</div>
 );
 	
 const Block = props => <div>{props.children}</div>;
@@ -23,14 +23,22 @@ function App() {
   return (
     <div className="App">
       <Tweet>
-				<Avatar />
-				<Message />
-				<HandleName />
-				<TimePosted />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
+				<InlineBlock>
+					<Avatar />
+				</InlineBlock>
+				<InlineBlock>
+					<Block>
+						<Message />
+						<NameWithHandle />
+						<Time />
+					</Block>
+					<Block>
+						<Button />
+						<Button />
+						<Button />
+						<Button />
+					</Block>
+				</InlineBlock>
 			</Tweet>
     </div>
   );
