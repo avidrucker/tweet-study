2fec1a1e41ed7b3895433f076ba10e0899c41e7e
Avi <avidrucker@gmail.com>
2020-03-24 20:48:57 -0400

    feat parameterize tweet cmpnt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import React from 'react';
 import './App.css';
 
 const Tweet = props => (
-	<div className={props.className}>{props.children}</div>);
+	<Block className="flex pa3 measure-narrow ba b--black ma3">
+
+		<InlineBlock className="w-20 flex flex-column items-center ">
+			<Block className="bg-lightest-blue tc f1 w3 h3 pa1">
+				<Avatar symbolIn={props.symbol} />
+			</Block>
+		</InlineBlock>
+
+		<InlineBlock className="w-80 f4">
+
+			<Block className="flex flex-row">
+
+				<InlineBlock className="bg-washed-green">
+					<NameWithHandle textIn={`${props.name} @${props.handle}`} />
+				</InlineBlock>
+
+				<InlineBlock>
+					<Inline className="ma3">•</Inline>
+				</InlineBlock>
+
+				<InlineBlock className="bg-washed-yellow">
+					<Time textIn={`${props.time}`}/>
+				</InlineBlock>
+
+			</Block>
+
+			<Block className="mv2 pv1">
+				<InlineBlock className="bg-washed-purple">
+					<Message textIn={`${props.message}`} />
+				</InlineBlock>
+			</Block>
+
+			<ButtonList />
+
+		</InlineBlock>
+
+	</Block>);
 
 const Avatar = props => <span>{props.symbolIn}</span>;
 	
@@ -24,49 +60,35 @@ const InlineBlock = props => (
 );
 	
 const Block = props => (
-	<div className={props.className + " db"}>{props.children}</div>);
+	<section className={props.className + " db"}>{props.children}</section>);
 
-function App() {
+function ButtonList() {
 	const symbols = ["<--", "@", "<3", "..."];
 	const buttons = symbols.map((x,i) =>
 		<Inline key={i.toString()} className="bg-washed-red mh3">
 			<Button symbolIn={x} />
 		</Inline>
   );
+	return (<Block className="flex">{buttons}</Block>);
+}
 
+function App() {
   return (
-    <div className="f3 border-box">
-      <Tweet className="flex pa3 measure-narrow ba b--black ma3">
+    <main className="f3 border-box">
+      <Tweet
+				symbol={"&"}
+				name="Joe"
+				handle="MightyJoe"
+				time="3h ago"
+				message="I live for cucumbers." />
 
-				<InlineBlock className="w-20 flex flex-column items-center ">
-					<Block className="bg-lightest-blue tc f1 w3 h3 pa1">
-						<Avatar symbolIn={"&"} />
-					</Block>
-				</InlineBlock>
-
-				<InlineBlock className="w-80 f4">
-					<Block className="flex flex-row">
-						<InlineBlock className="bg-washed-green">
-							<NameWithHandle textIn="Your Name @Handle Name" />
-						</InlineBlock>
-						<InlineBlock>
-							<Inline className="ma3">•</Inline>
-						</InlineBlock>
-						<InlineBlock className="bg-washed-yellow">
-							<Time textIn="3h ago"/>
-						</InlineBlock>
-					</Block>
-					<Block className="mv2 pv1">
-						<InlineBlock className="bg-washed-purple">
-							<Message textIn="Insightful message" />
-						</InlineBlock>
-					</Block>
-					<Block className="flex">
-						{buttons}
-					</Block>
-				</InlineBlock>
-			</Tweet>
-    </div>
+			<Tweet
+				symbol={"J"}
+				name="Jenny"
+				handle="JnEfrmDaBlak"
+				time="15min ago"
+				message="I'm still Jenny from the block yo" />
+    </main>
   );
 }
 
